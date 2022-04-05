9b704b02b75411741b9888ef94b4648d9196094e
Fran Yoga <ing.francisco.oliva@gmail.com>
2022-04-05 19:22:27 -0300

    mock contracts to test

--- scripts/helpful_script.py
+++ scripts/helpful_script.py
@@ -2,8 +2,8 @@ from brownie import network, config, accounts, MockV3Aggregator
 from web3 import Web3
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development","ganache-local"]
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 def get_account():
     # diferentes formas de settear accounts
     #1
@@ -26,6 +26,6 @@ def deploy_mocks():
         print("Deploying Mocks....")
         if len(MockV3Aggregator) <= 0:
             print(f"mock V3 aggregator len {len(MockV3Aggregator)}")
-            MockV3Aggregator.deploy(DECIMALS,Web3.toWei(STARTING_PRICE,"ether"),{"from":get_account()})
+            MockV3Aggregator.deploy(DECIMALS,STARTING_PRICE,{"from":get_account()})
              
         print("....Mocks Deployed!")
